3f531ab53527f827356ae4140a302bb16cf7bc1d
Elton <eltonrazzini@hotmail.com>
2023-03-22 20:01:57 -0300

    alteracao de descricao de methodos https

--- routers/inscritos.js
+++ routers/inscritos.js
@@ -3,23 +3,23 @@ const router  = express.Router();
 
 router.get("/", function (req, res) {
 
-    res.send("try")
+    res.send("get all success");
 })
 router.get("/:id", function (req, res) {
 
-    res.send("try")
+    res.send("get this id sucess")
 })
 router.post("/", function (req, res) {
 
-    res.send("try")
+    res.send("post success")
 })
 router.patch("/:id", function (req, res) {
 
-    res.send("try")
+    res.send("update this id success")
 })
 router.delete("/:id", function (req, res) {
 
-    res.send("try")
+    res.send("delete this id success")
 })
 
 module.exports = router;
