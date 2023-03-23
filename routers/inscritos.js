fccdb9c6488ec7e69691e41b40e64cc5f1289137
Elton <eltonrazzini@hotmail.com>
2023-03-22 21:09:10 -0300

    fazendo get e post iniciais do mongoose

--- routers/inscritos.js
+++ routers/inscritos.js
@@ -1,18 +1,40 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const usuarios = require('../model/usuarios');
 
-router.get("/", function (req, res) {
 
-    res.send("get all success");
+router.get("/", async function (req, res) {
+
+    try {
+        const users = await usuarios.find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+
+    }
+   
 })
 router.get("/:id", function (req, res) {
 
     res.send("get this id sucess")
 })
-router.post("/", function (req, res) {
+router.post("/", async function (req, res) {
 
-    res.send("post success")
+    console.log(req.body)
+    const insertId = new usuarios({
+        nome : req.body.nome,
+        canal: req.body.canal
+    });
+    try {
+
+        const newUser = await insertId.save();
+        res.status(201).json(newUser)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+
 })
+
 router.patch("/:id", function (req, res) {
 
     res.send("update this id success")
