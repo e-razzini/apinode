7ec006163438d9e9e4b0ccd6737fef84c56cdcd9
Elton <eltonrazzini@hotmail.com>
2023-03-22 21:08:11 -0300

    erros de obj para json posisao no script app json

--- server.js
+++ server.js
@@ -2,17 +2,14 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+app.use(express.json());
 
 const inscritoRoute = require('./routers/inscritos.js');
 app.use("/inscritos",inscritoRoute);
 
 mongoose.connect(process.env.DATABASE_STRING,{useNewUrlParser:true,useUnifiedTopology:true});
 const db = mongoose.connection;
+
 db.on("error",(err)=> console.log("error"));
 db.once("open",() => console.log("conected success"));
-
-app.use(express.json());
-
-
-
-app.listen(3000,() => {console.log('start');})
+app.listen(3000,() => {console.log('start');}) 
